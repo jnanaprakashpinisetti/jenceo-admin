3f571043801b79a6c50dad1e09271bb4970e1e1b
jnanaprakash <prakash143.thinkspace@gmail.com>
2024-04-20 16:20:43 +0530

    Table Component added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from 'react';
+import invest from '../assets/invest-blue.svg';
 
 export default function Card(props) {
   return (
     <div className='card'>
-        <div className="card-header">
-           <h4><img src={props.invest} alt={props.investAlt} /> Investment</h4>
+      <div className="card-header">
+        <h6><img src={props.icon} alt={props.iconALt} /> {props.title}</h6>
+        <h3>{props.amount}</h3>
+      </div>
+      <div className="card-footer">
+        <div>
+          <span>{props.title1}</span>
+          <p> {props.amount1} </p>
         </div>
-        <div className="card-footer">
-            asdf
+        <div>
+          <span>{props.title2}</span>
+          <p> {props.amount2} </p>
         </div>
-      
+        <div>
+          <span>{props.title3}</span>
+          <p> {props.amount3} </p>
+        </div>
+      </div>
+
     </div>
   )
 }
---
+++ src/components/Table.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+export default function Table() {
+    return (
+        <>
+            <table className="table table-dark table-hover">
+                <thead>
+                    <tr>
+                        <th>S No</th>
+                        <th>Date</th>
+                        <th>Inves</th>
+                        <th>New Qty</th>
+                        <th>Old Qty</th>
+                        <th>Total Qty</th>
+                        <th>Sal Qty</th>
+                        <th>Sal Amt</th>
+                        <th>Exp</th>
+                        <th>Avg Pur</th>
+                        <th>Avg Sale</th>
+                        <th>Profit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+            
+                </tbody>
+            </table>
+        </>
+    )
+}

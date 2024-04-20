630243aea1b81c22fb9428de56635a2ea071c620
jnanaprakash <prakash143.thinkspace@gmail.com>
2024-04-20 07:41:26 +0530

    Left nav sliding funcanality done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Card from '../components/Card'
 
 export default function Dashboard() {
   return (
     <div className="layout-body">
-        dash board
-      
+
+      <h2>JenCeo Admin Dash Board</h2>
+      <div className="card-wrapper">
+        <NavLink to='Investments'>  <Card /></NavLink>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
     </div>
   )
 }

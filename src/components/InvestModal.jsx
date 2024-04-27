177188fc212b2d2608bc2487ab50685f1bbbb796
jnanaprakash <prakash143.thinkspace@gmail.com>
2024-04-27 14:45:14 +0530

    Removed Props prefix

--- src/components/InvestModal.jsx
+++ src/components/InvestModal.jsx
@@ -1,26 +1,33 @@
-export default function InvestModal(props) {
+export default function InvestModal({ 
+    modalClass,
+    cancleFun,
+    name,
+    amount,
+    date,
+    saveFun }) {
+
 
     return (
 
         <>
-            <div className={props.modalClass} id="investModal">
+            <div className={modalClass} id="investModal">
                 <div className="modal-dialog">
                     <div className="modal-content">
 
                         <div className="modal-header">
                             <h4 className="modal-title">Do you want to Save Details ?</h4>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" onClick={props.cancleFun}></button>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" onClick={cancleFun}></button>
                         </div>
 
                         <div className="modal-body">
-                            <p>Name: <strong> {props.name} </strong></p>
-                            <p>Amount: <strong>{props.amount}</strong></p>
-                            <p>Date: <strong>{props.date}</strong></p>
+                            <p>Name: <strong> {name} </strong></p>
+                            <p>Amount: <strong>{amount}</strong></p>
+                            <p>Date: <strong>{date}</strong></p>
                         </div>
 
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={props.cancleFun}>Cancle</button>
-                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={props.saveFun}>Save</button>
+                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={cancleFun}>Cancle</button>
+                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={saveFun}>Save</button>
                         </div>
 
                     </div>

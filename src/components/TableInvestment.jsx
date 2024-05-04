c94e9babb48750339e2114670caf6295dddab53d
jnanaprakash <prakash143.thinkspace@gmail.com>
2024-05-04 07:51:45 +0530

    Displaying data funcanality done

--- src/components/TableInvestment.jsx
+++ src/components/TableInvestment.jsx
@@ -1,63 +1,39 @@
 import React from 'react';
 import editIcon from '../assets/eidt.svg';
+import deleteIcon from '../assets/delete.svg';
 
-export default function TableInvestment() {
+export default function TableInvestment({
+    sno,
+    investor,
+    invest_date,
+    invest_amount,
+    invest_comments
+}) {
     return (
         <>
             <table className="table table-dark table-hover">
                 <thead>
                     <tr>
                         <th>S No</th>
+                        <th>Name</th>
                         <th>Date</th>
                         <th>Amount</th>
-                        <th>To</th>
-                        <th>Ref No</th>
-                        <th>Purpose</th>
                         <th>Comments</th>
-                        <th>Edit</th>
+                        <th className='action'>Edit</th>
+                        <th className='action'>Delete</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>1</td>
-                        <td>12-05-2024</td>
-                        <td>2000</td>
-                        <td>S.B.I</td>
-                        <td>123</td>
-                        <td>Invest</td>
-                        <td>Loremipusm</td>
-                        <td><img src={editIcon} alt="edit Icon" /></td>
+                        <td>{sno}</td>
+                        <td>{investor}</td>
+                        <td>{invest_date}</td>
+                        <td>{invest_amount}</td>
+                        <td>{invest_comments}</td>
+                        <td className='action' title='Edit'><img src={editIcon} alt="edit Icon" /></td>
+                        <td className='action' title='Delete'><img src={deleteIcon} alt="Delete Icon" /></td>
                     </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>12-05-2024</td>
-                        <td>2000</td>
-                        <td>S.B.I</td>
-                        <td>123</td>
-                        <td>Invest</td>
-                        <td>Loremipusm</td>
-                        <td><img src={editIcon} alt="edit Icon" /></td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>12-05-2024</td>
-                        <td>2000</td>
-                        <td>S.B.I</td>
-                        <td>123</td>
-                        <td>Invest</td>
-                        <td>Loremipusm</td>
-                        <td><img src={editIcon} alt="edit Icon" /></td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>12-05-2024</td>
-                        <td>2000</td>
-                        <td>S.B.I</td>
-                        <td>123</td>
-                        <td>Invest</td>
-                        <td>Loremipusm</td>
-                        <td><img src={editIcon} alt="edit Icon" /></td>
-                    </tr>
+
 
                 </tbody>
             </table>
--- src/pages/Investments.jsx
+++ src/pages/Investments.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import InvestForm from '../components/InvestForm'
-import Card from '../components/Card'
-import TableInvestment from '../components/TableInvestment'
+import React, { useState, useEffect } from 'react';
+import InvestForm from '../components/InvestForm';
+import Card from '../components/Card';
+import TableInvestment from '../components/TableInvestment';
+import editIcon from '../assets/eidt.svg';
+import deleteIcon from '../assets/delete.svg';
+
+import firebaseDB from '../firebase';
+
 
 export default function Investments() {
+
+  const [getInvestData, setGetInvestData] = useState({});
+
+  // displaying data in UI
+  useEffect(() => {
+    firebaseDB.child("Investments").on("value", details => {
+      setGetInvestData(details.val());
+      console.log(details.val())
+    })
+  }, [])
+
   return (
     <div className="layout-body">
       <div className="container-fluid investments">
         <div className="row">
           <div className="col-md-3">
-          <h4>Add Investment Details</h4>
+            <h4>Add Investment Details</h4>
             <InvestForm />
           </div>
           <div className="col-md-2">
-          <h4>Individual Details</h4>
+            <h4>Individual Details</h4>
             <Card
               amount={"Suresh"}
               title1={"Jan"}
@@ -41,11 +57,52 @@ export default function Investments() {
               title3={"Mar"}
               amount3={1000}
             />
-            </div>
-            <div className="col-md-7">
-              <h4>All Investment Details</h4>
+          </div>
+          <div className="col-md-7">
+            <h4>All Investment Details</h4>
 
-          <TableInvestment/>
+            {/* {Object.keys(getInvestData).map(item =>
+              <TableInvestment
+                key={item}
+                investor={getInvestData[item].investor}
+                invest_date={getInvestData[item].invest_date}
+                invest_amount={getInvestData[item].invest_amount}
+                invest_comments={getInvestData[item].invest_comments}
+              />
+            )} */}
+            <table className="table table-dark table-hover">
+              <thead>
+                <tr>
+                  <th className='sno'>S No</th>
+                  <th className='name'>Name</th>
+                  <th className='date'>Date</th>
+                  <th className='amount'>Amount</th>
+                  <th>Comments</th>
+                  <th className='action'>Edit</th>
+                  <th className='action'>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(getInvestData).map((item, index) =>
+                  <tr key={index}>
+                    <td className='sno'> {index + 1}</td>
+                    <td className='name'>{getInvestData[item].investor}</td>
+                    <td className='date'>{getInvestData[item].invest_date}</td>
+                    <td className='amount'>{getInvestData[item].invest_amount}</td>
+                    <td>{getInvestData[item].invest_comments}</td>
+                    <td className='action' title='Edit'><img src={editIcon} alt="edit Icon" /></td>
+                    <td className='action' title='Delete'><img src={deleteIcon} alt="Delete Icon" /></td>
+                  </tr>
+                )}
+                <tr>
+                  <td colSpan={3}>Total</td>
+                  <td colSpan={4}><strong></strong></td>
+                </tr>
+              </tbody>
+              
+            </table>
+
+
           </div>
         </div>
       </div>

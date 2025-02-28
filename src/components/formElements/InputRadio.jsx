b0bd491abec2863dcedf0fbe9009deeca984e917
jnanaprakash <prakash143.thinkspace@gmail.com>
2025-02-28 20:25:14 +0530

    Basic info Section validation done

--- src/components/formElements/InputRadio.jsx
+++ src/components/formElements/InputRadio.jsx
@@ -4,7 +4,7 @@ export default function InputRadio(props) {
     return (
         <>
             <div className="form-check form-check-inline">
-                <input type="radio" className="form-check-input" id={props.idName} name={props.inputName} value={props.value} /> {props.labelName}   
+                <input type="radio" className="form-check-input" id={props.idName} name={props.inputName} value={props.value} onChange={props.changeHandler}/> {props.labelName}   
                 <label className="form-check-label" htmlFor={props.htmlFor}> </label>
             </div>
         </>

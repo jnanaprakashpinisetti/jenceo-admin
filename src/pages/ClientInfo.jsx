68beb0a8be804db34b66ab9f1474c97b21c96bad
Jnana Prakash Pinisetti <35128182+jnanaprakashpinisetti@users.noreply.github.com>
2025-08-23 11:43:16 +0530

    Display client data funcanality done

--- src/pages/ClientInfo.jsx
+++ src/pages/ClientInfo.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ClientInfoForm from '../components/clientInfo/ClientInfoForm';
+import DisplayClient from '../components/clientInfo/DisplayClient';
+
 
 
 export default function ClientInfo() {
@@ -9,6 +11,9 @@ export default function ClientInfo() {
         <div className='row'>
           <div className='col-md-4'>
             <ClientInfoForm/>
+          </div>
+          <div className='col-md-8'>
+            <DisplayClient />
 
           </div>
         </div>

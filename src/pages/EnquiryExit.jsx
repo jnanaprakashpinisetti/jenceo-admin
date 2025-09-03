207cf776ee3f3077f83423973270c016c2d0ef8f
Jnana Prakash Pinisetti <35128182+jnanaprakashpinisetti@users.noreply.github.com>
2025-09-03 22:45:16 +0530

    Delete Enquiry display added

---
+++ src/pages/EnquiryExit.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DeletedEnquiriesDisplay from '../components/Enquiry/DeletedEnquiriesDisplay'
+
+
+export default function EnquiryExit() {
+  return (
+  <div className="layout-body">
+    <div className="row">
+        <div className="col-md-12">
+            <DeletedEnquiriesDisplay />
+        </div>
+    </div>
+    </div>
+  )
+}

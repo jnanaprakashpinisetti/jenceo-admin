6eb839aac7912af8f8d5d28b8e2a8bbdb379356b
jnanaprakash <prakash143.thinkspace@gmail.com>
2025-10-01 18:14:45 +0530

    Delete Expences added

---
+++ src/pages/ExpenceDelete.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PettyCashDeleteReport from '../components/PettyCash/PettyCashDeleteReport'
+
+
+
+export default function ExpenceDelete() {
+  return (
+    <div className="layout-body">
+      <PettyCashDeleteReport />
+    </div>
+  )
+}
--- src/pages/Operations.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Operations() {
-  return (
-    <div className="layout-body">
-      Operations
-    </div>
-  )
-}

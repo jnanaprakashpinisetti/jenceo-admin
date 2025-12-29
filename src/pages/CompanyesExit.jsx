cafc48824578c823baf99f5008fd55ab7f3a5e27
Jnana Prakash Pinisetti <35128182+jnanaprakashpinisetti@users.noreply.github.com>
2025-12-29 18:20:22 +0530

    Existing Companys file created and working fine

---
+++ src/pages/CompanyesExit.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import DisplayExitCompany from '../components/Companyes/DisplayExitCompany'
+
+export default function CompanyesExit() {
+  return (
+
+
+    <div className="layout-body">
+      <div className='row'>
+        <div className='col-md-12'>
+      <DisplayExitCompany />
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
